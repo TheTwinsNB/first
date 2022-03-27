2fb779ccedfc6056a947ffb51acde19f8005d32e
thetwins <1286233490@qq.com>
2022-03-26 20:02:11 -0700

    manage

--- myfirstdjango/firstapp/admin.py
+++ myfirstdjango/firstapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from firstapp.models import BookInfo, PersonInfo
+
+
+admin.site.register(BookInfo)
+admin.site.register(PersonInfo)
--- myfirstdjango/firstapp/models.py
+++ myfirstdjango/firstapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class PersonInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
